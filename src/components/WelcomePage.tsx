f0c999f56da6ccebc33595f1cb2f60a5d853b468
Emelie Strömberg <90707506+emeliestromberg@users.noreply.github.com>
2021-12-13 14:05:56 +0100

    Add files via upload

---
+++ src/components/WelcomePage.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "../App.css";
+import MainMenuNavigation from "./MainMenuNavigation";
+
+function App() {
+  return (
+    <div className="App">
+      <MainMenuNavigation />
+    </div>
+  );
+}
+
+export default App;
