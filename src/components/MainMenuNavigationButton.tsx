6db6a9bd33b3bbf6583900aa157a3d32d750fd48
Emelie Strömberg <90707506+emeliestromberg@users.noreply.github.com>
2021-12-14 12:16:39 +0100

    Add files via upload

---
+++ src/components/MainMenuNavigationButton.tsx
@@ -0,0 +1,18 @@
+import React, { FunctionComponent } from "react";
+
+interface Props {
+  buttontext: string;
+  link: string;
+}
+
+const MainMenuNavigationButton: FunctionComponent<Props> = (props) => {
+  return (
+    <li>
+      <div className="button button1 firstli">
+        <a href={props.link}>{props.buttontext}</a>
+      </div>
+    </li>
+  );
+};
+
+export default MainMenuNavigationButton;
---
+++ src/components/WelcomePage.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import "../App.css";
+import MainMenuNavigation from "./MainMenuNavigation";
+import "../styles/WelcomePage.css";
+//import {Link} from "react-router-dom";
+
+function WelcomePage() {
+  return (
+    <div className="pictureAndButton">
+      <div className="welcome">
+        <h2 className="chatbubble">Welcome to</h2>
+        <br />
+        <h2 className="forre">Forefronts Lunch Roulette</h2>
+      </div>
+
+      <div className="enterButton">
+        <h2>Click here to enter the tent</h2>
+        <br />
+        <br />
+        {<a href="src/components/MainMenuNavigation.tsx">ENTER</a>}
+       {/* <Link to={"./MainMenuNavigation"}>ENTER</Link> */}
+      </div>
+    </div>
+  );
+}
+
+export default WelcomePage;
