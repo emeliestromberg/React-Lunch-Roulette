9023eac7ffb11dd753bf2987280ddf731bbc44fb
emeliestromberg <90707506+emeliestromberg@users.noreply.github.com>
2021-12-20 13:27:08 +0100

    .

---
+++ src/components/buttons/SpinTheWheelButton.tsx
@@ -0,0 +1,19 @@
+import React, { FunctionComponent } from "react";
+import "../styles/SpinTheWheelPage.css";
+
+interface Props {
+  buttontext: string;
+  link: string;
+}
+
+const MainMenuNavigationButton: FunctionComponent<Props> = (props) => {
+  return (
+    <li>
+      <div className="button button1 playbutton">
+        <a href={props.link}>{props.buttontext}</a>
+      </div>
+    </li>
+  );
+};
+
+export default MainMenuNavigationButton;
