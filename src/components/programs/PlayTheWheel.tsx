7e0224962afe7d3afa756ca73cb9cba5a86a3ed7
emeliestromberg <90707506+emeliestromberg@users.noreply.github.com>
2021-12-20 16:37:56 +0100

    play the wheel mannen

---
+++ src/components/programs/PlayTheWheel.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+interface Props {
+  buttontext: string;
+  link: string;
+}
+
+const PlayTheWheel = (props) => {
+
+
+
+  
+  return (
+    <>
+    </>
+  );
+};
+
+export default PlayTheWheel;
