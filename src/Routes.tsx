95f66fed05172f4d30800468639f118fd6e32041
emeliestromberg <90707506+emeliestromberg@users.noreply.github.com>
2021-12-20 13:11:11 +0100

    Update Routes.tsx

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,23 +1,57 @@
-import React from "react";
+import React, { FunctionComponent } from "react";
 import ReactDOM from "react-dom";
 import WelcomePage from "./components/WelcomePage";
-//import RemovePersonPage from "./components/RemovePersonPage"
+//import MainMenuNavigation from "./components/MainMenuNavigation";
+import SeeListPage from "./components/SeeListPage";
+import SpinTheWheelPage from "./components/SpinTheWheelPage";
+import RemovePersonPage from "./components/RemovePersonPage";
+import AddPersonPage from "./components/AddPersonPage";
+
 import {
   BrowserRouter as Router,
-  Routes as Switch,
+  Route as Switch,
   Route,
   useParams,
 } from "react-router-dom";
 
-const Routes: React.FunctionComponent = () => {
+const Routes: FunctionComponent = () => {
   return (
     <Router>
       <Switch>
-        <Route path="/test" element={<p>hej</p>}></Route>
-        <Route path="/" element={<WelcomePage />}></Route>
-      {/*  <Route path="/RemovePerson" element={<RemovePersonPage />}></Route> */}
+        <Route path="/home" element={<WelcomePage />}></Route>
+        <Route path="/SeeList" element={<SeeListPage />}></Route>
+        <Route path="/SpinTheWheel" element={<SpinTheWheelPage />}></Route>
+        <Route path="/RemovePerson" element={<RemovePersonPage />}></Route>
+        <Route path="/AddPerson" element={<AddPersonPage />}></Route>
       </Switch>
     </Router>
   );
 };
+
 export default Routes;
+
+/*
+
+
+<Router>
+      <Switch>
+        <Route path="/home">
+          <WelcomePage />
+        </Route>
+        <Route path="/SeeList">
+          <SeeListPage />
+        </Route>
+        <Route path="/SpinTheWheel">
+          <SpinTheWheelPage />
+        </Route>
+        <Route path="/RemovePerson">
+          <RemovePersonPage />
+        </Route>
+        <Route path="/AddPerson">
+          <AddPersonPage />
+        </Route>
+      </Switch>
+    </Router>
+
+
+*/
