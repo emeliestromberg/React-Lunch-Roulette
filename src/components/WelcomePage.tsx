67bd23e9abf6e3890830e8afddc87ebdc2987020
Nora-Amundsson <nora.amundsson@outlook.com>
2021-12-20 13:38:18 +0100

    welcomepage background no body

--- src/components/WelcomePage.tsx
+++ src/components/WelcomePage.tsx
@@ -2,23 +2,24 @@ import React from "react";
 import "../App.css";
 import MainMenuNavigation from "./MainMenuNavigation";
 import "../styles/WelcomePage.css";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 function WelcomePage() {
   return (
     <div className="pictureAndButton">
+      <div className="welcomePageBackground" style={{ backgroundImage: WelcomePageBackground }}>
+        <div className="welcome">
+          <h2 className="chatbubble">Welcome to</h2>
+          <br />
+          <h2 className="forre">Forefronts Lunch Roulette</h2>
+        </div>
 
-      <div className="welcome">
-        <h2 className="chatbubble">Welcome to</h2>
-        <br />
-        <h2 className="forre">Forefronts Lunch Roulette</h2>
-      </div>
-
-      <div className="enterButton">
-        <h2>Click here to enter the tent</h2>
-        <br />
-        <br />
-        <Link to={"/Home"}>ENTER</Link>
+        <div className="enterButton">
+          <h2>Click here to enter the tent</h2>
+          <br />
+          <br />
+          <Link to={"/Home"}>ENTER</Link>
+        </div>
       </div>
     </div>
   );
