cda6958b103c9732d9f0f0156e3209b289d74016
emeliestromberg <90707506+emeliestromberg@users.noreply.github.com>
2021-12-21 16:40:40 +0100

    .

--- src/components/programs/ParticipantList.tsx
+++ src/components/programs/ParticipantList.tsx
@@ -1,6 +1,10 @@
 import { useState } from "react";
 import React from "react";
 
+let plist: Array<string>;
+
+let plistplay: Array<string>;
+
 const ParticipantList = () => {
   const [list, setlist] = useState([
     { name: "Karin Odin", id: 1 },
@@ -8,6 +12,34 @@ const ParticipantList = () => {
     { name: "Sofia Rokkones", id: 3 },
     {},
   ]);
+
+const fsw = require("fsw");
+
+let text = "Something you want to write in";
+
+fsw.writeFileSync("Participants.txt", text, function (err: any) {
+  if (err) {
+    return console.log("error");
+  }
+});
+
+  
+
+const fsr = require("fsr");
+
+fsr.readFile(
+  "Participants.txt",
+  function (err: any, data: { toString: () => string }) {
+    if (err) throw err;
+
+    const arr = data.toString().replace(/\r\n/g, "\n").split("\n");
+
+    for (let i of arr) {
+      console.log(i);
+    }
+  }
+);
+
 
   return (
     <>
--- src/components/programs/PlayTheWheel.tsx
+++ src/components/programs/PlayTheWheel.tsx
@@ -3,11 +3,14 @@ import React from "react";
 
 const PlayTheWheel = () => {
 
-Math.random()
+Math.random() 
 
 
   return (
     <>
+    <h2>May I present to you, three people who can go to lunch together:</h2>
+    <p>{ lucnhgroup }</p>
+
     </>
   );
 };
